848899bd1d49f3048c9e6117beb8801c44e689b3
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-03-10 16:06:55 +0900

    feat: add prisma migration and generate client

--- todo-api/src/index.ts
+++ todo-api/src/index.ts
@@ -1,8 +1,11 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { PrismaClient } from './generated/prisma/client.js'
 
 const app = new Hono()
+
+const prisma = new PrismaClient()
 
 interface Todo {
   id: number
